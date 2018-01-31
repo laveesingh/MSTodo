e43690982b54803d03a7bea2c7998b9a04d336f8
lavee singh <lavees96@gmail.com>
2018-02-01 01:51:33 +0530

    modified readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple ToDo Application
+
+### Get it running locally
+
+* Install meteor
+```bash
+curl https://install.meteor.com/ | sh
+```
+
+* Clone the repo and run localy
+```bash
+git clone https://github.com/laveesingh/MSTodo
+meteor npm install
+meteor run
+```
+
+* Go check `localhost:3000`, the app should be running
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -25,9 +25,7 @@ export default class Task extends Component {
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked}
         />
-        <span className='text'>
-          {this.props.task.text}
-        </span>
+        <span className="text">{this.props.task.text}</span>
       </li>
     )
   }
